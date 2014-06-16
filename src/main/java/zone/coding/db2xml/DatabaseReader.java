0516b566bef8a77a8c588b911fdd00c55f9ed426
Manuel Reithuber <git@manuel.reithuber.net>
2014-06-16 12:40:22 +0200

    added 'exclude' key to jdbc.properties.example (and implemented excluding tables)

--- src/main/java/zone/coding/db2xml/DatabaseReader.java
+++ src/main/java/zone/coding/db2xml/DatabaseReader.java
@@ -5,11 +5,20 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class DatabaseReader {
 	public static void read(Connection db, XmlWriter writer, Properties props) throws SQLException {
+		Set<String> excludedTables = _getExcludedList(props);
+		System.out.println("Extracting tables:");
 		for (String tableName: JdbcHelper.listTables(db)) {
-			_writeTable(db, writer, tableName, props);
+			if (!excludedTables.contains(tableName)) {
+				_writeTable(db, writer, tableName, props);
+			}
+			else {
+				System.out.println("\t'"+tableName+"' (excluded)");
+			}
 		}
 		System.out.println("done...");
 	}
@@ -18,8 +27,19 @@ public class DatabaseReader {
 		read(db, writer, new Properties());
 	}
 
+	private static Set<String> _getExcludedList(Properties props) {
+		Set<String> rc = new TreeSet<String>();
+		String list[] = props.getProperty("excluded", "").split(",");
+
+		for (String tableName: list) {
+			rc.add(tableName);
+		}
+
+		return rc;
+	}
+
 	private static void _writeTable(Connection db, XmlWriter writer, String tableName, Properties props) throws SQLException {
-		System.out.println("Extracting table '"+tableName+"'");
+		System.out.println("\t'"+tableName+"'");
 		boolean includeSchema = new Boolean(props.getProperty("includeSchema", "true"));
 		boolean includeData = new Boolean(props.getProperty("includeData", "true"));
 
