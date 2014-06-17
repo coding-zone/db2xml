d94ed91879f81af14168f5057028dbe80397c775
Manuel Reithuber <git@manuel.reithuber.net>
2014-06-17 08:54:40 +0200

    documented Column

--- src/main/java/zone/coding/db2xml/entity/Column.java
+++ src/main/java/zone/coding/db2xml/entity/Column.java
@@ -1,6 +1,19 @@
 package zone.coding.db2xml.entity;
 
+/**
+ * Stores the meta data of a database table column
+ */
 public class Column {
+	/**
+	 * Constructor
+	 * @param name Column name
+	 * @param type Column data type (as reported by the database)
+	 * @param standardType One of {@link java.sql.Types}
+	 * @param size Datatype size
+	 * @param nullable Nullability (true if nullable, false if not, null if undetermined)
+	 * @param comment Column comment
+	 * @param defaultValue Default value (plain strings are enclosed in single quotes, computed values + numbers aren't
+	 */
 	public Column(String name, String type, int standardType, Integer size, Boolean nullable, String comment, String defaultValue) {
 		m_name = name;
 		m_type = type;
@@ -11,30 +24,58 @@ public class Column {
 		m_default = defaultValue;
 	}
 
+	/**
+	 * Column name getter
+	 * @return Column name
+	 */
 	public String getName() {
 		return m_name;
 	}
 
+	/**
+	 * Column type getter
+	 * @return Column type (might be DBMS specific)
+	 */
 	public String getType() {
 		return m_type;
 	}
 
+	/**
+	 * {@link java.sql.Types} getter
+	 * @return One of the types defined in {@link java.sql.Types}
+	 */
 	public int getStandardType() {
 		return m_standardType;
 	}
 
+	/**
+	 * Column size getter (length for varchar, storage size for numbers, etc.)
+	 * @return Column size
+	 */
 	public Integer getSize() {
 		return m_size;
 	}
 
+	/**
+	 * Returns whether or not the column is nullable
+	 * @return true/false to indicate nullability of the column. <code>null</code> if undetermined
+	 */
 	public Boolean isNullable() {
 		return m_nullable;
 	}
 
+	/**
+	 * Returns the column's comment (or null if there isn't any)
+	 * @return Column comment String
+	 */
 	public String getComment() {
 		return m_comment;
 	}
 
+	/**
+	 * Default value getter
+	 * @return Column's default value (or null if there is none). String values are encased in single quotes.
+	 */
 	public String getDefault() {
 		return m_default;
 	}
